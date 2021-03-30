02b251d3c3ec0a26a7fdd911f60c138463c69ecb
Olga Lebedeva <lebedevaolga021@gmail.com>
2021-03-30 22:18:17 +0300

    no message

--- src/levels.ts
+++ src/levels.ts
@@ -45,7 +45,7 @@ export class LevelOne {
         this.option3 = three;
     };
 };
-export const userSelect1 = new LevelOne("a. Buy it again as fast as possible (3 points)", "b. Try to find it as fast as possible (2 points)", "c. Forget about it, weekends is coming! (1 point)");
+export const userSelect1 = new LevelOne("1. Buy it again as fast as possible (3 points)", "2. Try to find it as fast as possible (2 points)", "3. Forget about it, weekends is coming! (1 point)");
 
 
 
@@ -60,7 +60,7 @@ export class LevelTwo {
         this.option3 = three;
     };
 };
-export const userSelect2 = new LevelTwo("a.Time to check it and order via the Internet - check all online book shops (3 points)", "b.Check only free download version (2 points)", "c. I'll decide later, I am reluctant now (0 points)");
+export const userSelect2 = new LevelTwo("1.Time to check it and order via the Internet - check all online book shops (3 points)", "2.Check only free download version (2 points)", "3. I'll decide later, I am reluctant now (0 points)");
 
 
 
@@ -75,4 +75,4 @@ export class LevelThree {
         this.option3 = three;
     };
 };
-export const userSelect3 = new LevelThree("a.Ride by taxi now! I must to catch it until the shop will close. (3 points)", "b. Ride by bus, probably will be in time (2 points)", "c. Ok, not now! will see on Sunday evening  (0 points)");
+export const userSelect3 = new LevelThree("1.Ride by taxi now! I must to catch it until the shop will close. (3 points)", "2. Ride by bus, probably will be in time (2 points)", "3. Ok, not now! will see on Sunday evening  (0 points)");
