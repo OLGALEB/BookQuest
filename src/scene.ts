d88482f3f74bd2e27a913a33943525f2462b8e7d
Olga Lebedeva <lebedevaolga021@gmail.com>
2021-03-30 23:13:01 +0300

    total result for user

--- src/scene.ts
+++ src/scene.ts
@@ -1,14 +1,16 @@
-import {  } from "./userinterface"
+import { userGameProgress } from "./userinterface"
 
-let GoodResult:string = "Fine! You had time to buy the book, the exams have been passed"
-let MediumResult:string="You have a chance to pass the exams!"
-let BadResult:string = "Oh no! You did not have time to buy the book and exams have not been passed"
+let GoodResult: string = "Fine! You had time to buy the book, the exams have been passed"
+let MediumResult: string = "You have a chance to pass the exams!"
+let BadResult: string = "Oh no! You did not have time to buy the book and exams have not been passed"
 
-export function userResult(){
-    if())
-    }else{
-        ));
-        return;
-    };
-    
-};
+
+export function totalScore() {
+    if (userGameProgress > 6) { 
+        console.log(GoodResult) 
+    }else if (userGameProgress > 4){
+        console.log(MediumResult)
+    } else if (userGameProgress < 4){
+        console.log(BadResult)
+    }
+}
