eeb7bc68b2c2562205c945cf3c243e7d4c620fe4
Olga Lebedeva <lebedevaolga021@gmail.com>
2021-03-30 22:55:25 +0300

    scene + user progress update fix

--- src/userinterface.ts
+++ src/userinterface.ts
@@ -1,17 +1,87 @@
-const userInput = require('prompt-sync')(); 
-import { userSelect1, userSelect2, userSelect3, lvlNames}  from "./levels";
+const userInput = require('prompt-sync')();
+import { userSelect1, userSelect2, userSelect3, lvlNames } from "./levels";
 
-export let userGameProgress:number = 0;
+export let userInput1: number = 0;
+export let secondLevelOption: number = 0;
+export let thirdLevelOption: number = 0;
+export let userGameProgress: number = 0;
 
-export function firstLevelQuestion(){
+export function firstLevelQuestion() {
    return console.log(lvlNames.level1);
 };
-let userAnswer1 = userInput ("Please enter your сhoice a-c")
-export function userChoiceFirst(){
 
-    
-    return userAnswer1 + 
-    ${userSelect1 .option1}
-    ${userSelect1 .option2}
-    ${userSelect1 .option3}
-    
+
+
+
+export function firstUserChoice() {
+   userInput1 = userInput(`
+   ${userSelect1.option1}
+   ${userSelect1.option2}
+   ${userSelect1.option3}
+   
+   Make your choice - select 1,2 or 3`);
+
+}
+
+while (userInput1 > 3 || userInput1 < 1) {//прогресс от 1й сцены
+   userInput1 = userInput("Incorrect number! Choose 1-3")
+};
+
+if (userInput1 == 1) {
+   userGameProgress += 3
+} else if (userInput1 == 2) {
+   userGameProgress += 2
+} else if (userInput1 == 3) {
+   userGameProgress += 1
+};
+
+
+
+
+
+export function secondUserChoice() {//прогресс от 2й сцены
+   userInput1 = userInput(`
+   ${userSelect2.option1}
+   ${userSelect2.option2}
+   ${userSelect2.option3}
+   
+   Make your choice - select 1,2 or 3`);
+
+}
+
+while (userInput1 > 3 || userInput1 < 1) {
+   userInput1 = userInput("Incorrect number! Choose 1-3")
+};
+
+if (userInput1 == 1) {
+   userGameProgress += 3
+} else if (userInput1 == 2) {
+   userGameProgress += 2
+} else if (userInput1 == 3) {
+   userGameProgress += 0
+};
+
+
+
+
+export function thirdUserChoice() { //прогресс от 3й сцены
+   userInput1 = userInput(`
+   ${userSelect3.option1}
+   ${userSelect3.option2}
+   ${userSelect3.option3}
+   
+   Make your choice - select 1,2 or 3`);
+
+}
+
+while (userInput1 > 3 || userInput1 < 1) {
+   userInput1 = userInput("Incorrect number! Choose 1-3")
+};
+
+if (userInput1 == 1) {
+   userGameProgress += 3
+} else if (userInput1 == 2) {
+   userGameProgress += 2
+} else if (userInput1 == 3) {
+   userGameProgress += 0
+};
